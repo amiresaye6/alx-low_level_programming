7b2f463ee6edabe9b38a714612e1c0c14c8b0e63
Amir Alsayed <amyralsyd367@gmail.com>
2023-03-16 16:49:50 +0000

    inintial commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
+int mani(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
