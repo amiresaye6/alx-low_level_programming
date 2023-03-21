d869c8c302963adee6cfb2eb813b43f327aab4b3
Amir Alsayed <amyralsyd367@gmail.com>
2023-03-21 21:47:03 +0000

    commit #7

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
+/**
  * print_to_98 - prints the numbers from n to 98
  * @n: the number to start from
  */
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -29,7 +29,8 @@ void times_table(void)
 				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
-		}
+			}
 		_putchar('\n');
+		}
 	}
 }
