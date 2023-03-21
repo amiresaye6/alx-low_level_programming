34ebc2b297dffaf5e348dc6843aae9d05fdb96cf
Amir Alsayed <amyralsyd367@gmail.com>
2023-03-21 18:43:21 +0000

    test commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for(c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	return (0);
+}
