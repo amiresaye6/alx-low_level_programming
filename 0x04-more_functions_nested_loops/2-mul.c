8776e0d0e2844ffed34608bc729b63659e051861
Amir Alsayed <amyralsyd367@gmail.com>
2023-03-23 14:58:06 +0000

    commit #1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multpy two numbers
+ *
+ *@a: the first digit to multbly
+ *@b: the second digit to multly
+ *
+ * Return: a multbyl b
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+	return (res);
+}
