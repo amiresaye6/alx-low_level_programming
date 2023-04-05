8a459047c5e69d22bd745b4f07cf0d4a13f4afcf
Amir Alsayed <amyralsyd367@gmail.com>
2023-04-05 15:39:36 +0000

    commit #1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,11 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	if (s[i] == '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(s[i]);
+	_puts_recursion(s + 1);
 }
