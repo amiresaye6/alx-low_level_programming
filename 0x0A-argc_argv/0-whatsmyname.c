c4eddb892b7882fb5925e9d3b38048ea144bcb18
Amir Alsayed <amyralsyd367@gmail.com>
2023-04-08 14:17:06 +0000

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name evin if it is changed
+ * @argc: intger
+ * @argv: intger
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
