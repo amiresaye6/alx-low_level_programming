3845dc5f57118b9e5887e51b23c17e9330b47562
Amir <amyralsyd367@gmail.com>
2023-05-08 21:57:54 +0000

    final commit :((

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -33,7 +33,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (f == -1)
 		return (-1);
 	if (text_content != NULL)
-		l = write(fd, text_content, _strlen(text_content));
+		l = write(f, text_content, _strlen(text_content));
 	close(f);
 	if (l == -1)
 		return (-1);
