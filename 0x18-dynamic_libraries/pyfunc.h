e3447871fd73eed542850e5708383dcf9a035917
Amir Alsayed <amyralsyd367@gmail.com>
2023-08-21 16:42:35 +0300

    lets gooo

---
+++ 0x18-dynamic_libraries/pyfunc.h
@@ -0,0 +1,11 @@
+#ifndef _PYFUNC_H
+#define _PYFUNC_H
+
+// these functions are for the exra task in pyfincs.c
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif
