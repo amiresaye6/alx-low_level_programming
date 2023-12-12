143531429d7788e2b77b423eada17771ac4556be
Amir Alsayed <amyralsyd367@gmail.com>
2023-12-12 17:15:30 +0200

    debuging

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -4,7 +4,7 @@
  * binary_search - searches for a value in an array of integers using
  * the Binary search algorithm
  *
- * @array: pointer to the first elment to start from (int)
+ * @array: pointer to the first elment (int)
  * @size: the size of this array (size_t)
  * @value: the value to search for (int)
  *
@@ -21,6 +21,8 @@ int binary_search(int *array, size_t size, int value)
 		{
 			size_t middle = left + (right - left) / 2;
 
+			print_array(array, left, right);
+
 			if (array[middle] == value)
 				return (middle);
 			if (array[middle] > value)
@@ -31,4 +33,29 @@ int binary_search(int *array, size_t size, int value)
 	}
 
 	return (-1);
+}
+
+/**
+ * print_array - helper function to print array
+ * @array: pointer to the first elment (int)
+ * @start: the starting index (size_t)
+ * @end: the ending index (size_t)
+ *
+ *Return: (void)
+*/
+
+void print_array(int *array, size_t start, size_t end)
+{
+	char *delemeter = " ";
+
+	if (array != NULL)
+	{
+		printf("Searching in array:");
+		while (start <= end)
+		{
+			printf("%s%d", delemeter, array[start++]);
+			delemeter = ", ";
+		}
+		printf("\n");
+	}
 }
