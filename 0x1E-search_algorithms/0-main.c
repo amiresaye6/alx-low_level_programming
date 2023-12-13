b728cd34be48126d1da03e3c9fa70066c7502400
Amir Alsayed <amyralsyd367@gmail.com>
2023-12-13 15:23:37 +0200

    interpolation search

--- 0x1E-search_algorithms/0-main.c
+++ 0x1E-search_algorithms/0-main.c
@@ -10,12 +10,13 @@
 int main(void)
 {
     int array[] = {
-        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9, 8, 5, 2, 0, 0, 90
     };
     size_t size = sizeof(array) / sizeof(array[0]);
 
     printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
     printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
     printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+    printf("found the number of amir in index %d\n", linear_search(array, 16, 2));
     return (EXIT_SUCCESS);
 }
---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+#include <math.h>
+/**
+ * interpolation_search - searches for a value in an array of integers using
+ * the Interpolation search algorithm
+ *
+ * @array: pointer to the first elment (int)
+ * @size: the size of this array (size_t)
+ * @value: the value to search for (int)
+ *
+ * Return: the index where it find the value or -1 if failed
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low = 0, high = size - 1;
+
+	if (array == NULL || size < 1)
+		return (-1);
+
+	while (1)
+	{
+		size_t pos = low + (((double)(high - low) /
+		(array[high] - array[low])) * (value - array[low]));
+
+		if (pos > size - 1)
+		{
+			printf("Value checked array[%lu] is out of range\n", pos);
+			return (-1);
+		}
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		if (array[pos] == value)
+			return (pos);
+		high = pos - 1;
+	}
+	return (-1);
+}
