50f986f053160b45705031b18fe7fe7b3d2bd8aa
Amir Alsayed <amyralsyd367@gmail.com>
2023-12-13 15:34:02 +0200

    debuging

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -17,7 +17,7 @@ int interpolation_search(int *array, size_t size, int value)
 	if (array == NULL || size < 1)
 		return (-1);
 
-	while (1)
+	while (high >= low)
 	{
 		size_t pos = low + (((double)(high - low) /
 		(array[high] - array[low])) * (value - array[low]));
@@ -33,7 +33,7 @@ int interpolation_search(int *array, size_t size, int value)
 		if (array[pos] > value)
 			high = pos + 1;
 		else
-			high = pos - 1;
+			low = pos - 1;
 	}
 	return (-1);
 }
